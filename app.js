95dd9f37cd927e7146d4502b68ea63a6fad21cd2
Sophie Engobor <sophietengobor@gmail.com>
2022-02-15 12:59:12 -0600

    All done hope it's right lol, we'll see

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
-let friends = ['Jovanna', 'Jarita', 'Grace', 'Onyx', 'Bri'];
+document.addEventListener("DOMContentLoaded", function () {
+    const newDiv = document.createElement("div");
+    document.body.appendChild(newDiv);
 
-for (let i = 0; i < friends.length; i++) {
-    for (let n = 99; n >= 1; n--) {
-
-        if (n == 2) {
-            console.log( n + ' lines of code in the file, ' + n + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (n - 1) + 'lines of code in the file')
-
-        } else if (n > 2) {
-            console.log( n + ' lines of code in the file,' + n + ' lines of code;' + friends[i] + ' strikes one out, clears it all out, ' + (n - 1) + 'lines of code in the file')
-
-        } else {
-            console.log( n + ' line of code in the file,' + n + ' line of code;' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file')
-        }
-
+    document.getElementById(btn.id).onclick = function () {
+        listFriends();
     }
-} 
+
+
+    let friendArray = ["Jovanna", "Jarita", "Grace", "Onyx", "Bri"];
+
+
+    function listFriends() {
+        for (let i = 0; i < friendArray.length; i++) {
+            let friendsDiv = document.createElement("div");
+            friendsDiv.className = "friend"
+            let friendsName = document.createElement("h3");
+            friendsName.innerHTML = friendArray[i];
+            document.body.appendChild(friendsDiv);
+            friendsDiv.appendChild(friendsName);
+
+            for (let n = 99; n >= 1; n--) {
+                let words = document.createElement("p");
+                friendsDiv.appendChild(words);
+
+                if (n == 2) {
+                    words.innerHTML = n + ' lines of code in the file, ' + n + ' lines of code; ' + friendArray[i] + ' strikes one out, clears it all out, ' + (n - 1) + ' lines of code in the file.'
+
+                } else if (n > 2) {
+                    words.innerHTML = n + ' lines of code in the file, ' + n + ' lines of code; ' + friendArray[i] + ' strikes one out, clears it all out, ' + (n - 1) + ' lines of code in the file.'
+
+                } else {
+                    words.innerHTML = n + ' line of code in the file, ' + n + ' line of code; ' + friendArray[i] + ' strikes one out, clears it all out, no more lines of code in the file.'
+                }
+
+            }
+        }
+    }
+});
+
+
